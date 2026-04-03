06d8275e1bf8c13e59470a4ade84114bf4c6f5a5
Avinash <avinashhankur@gmail.com>
2026-04-03 10:10:16 +0530

    unused vars with _

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    ignores: ['dist/**', 'node_modules/**', 'src/generated/**'],
+    ignores: ["dist/**", "node_modules/**", "src/generated/**"],
   },
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     rules: {
-      semi: 'error',
-      'prefer-const': 'error',
+      semi: "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ]);
